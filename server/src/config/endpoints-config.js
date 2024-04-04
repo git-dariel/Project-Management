0be9c1099217c686e703028fe7cc32949cba2379
git-dariel <104293978+git-dariel@users.noreply.github.com>
2024-04-04 18:28:38 +0800

    add stage service and route

--- server/src/config/endpoints-config.js
+++ server/src/config/endpoints-config.js
@@ -1,30 +1,35 @@
 module.exports = {
-    API_ENDPOINTS: {
-        MAIN: {
-            DEFAULT: "/",
-        },
-        // user endpoints
-        USER_REGISTER: {
-            GET: "/users",
-            POST: "/users/register",
-            GET_UPDATE_DELETE_BY_ID: "/users/:id",
-        },
-        USER_LOGIN: {
-            GET: "/current",
-            POST: "/users/login"
-        },
-        // project endpoints
-        PROJECT: {
-            GET: "/projects",
-            POST: "/projects",
-            GET_UPDATE_DELETE_BY_ID: "/projects/:id",
-            ADD_MEMBER: "/projects/:project_id/member"
-        },
-        // project members endpoints
-        PROJECT_MEMBER: {
-            GET: "/:project_id",
-            DEACTIVATE: "/deactivate/:project_id/:user_id",
-            ACTIVATE: "/activate/:project_id/:user_id",
-        }
-    }
-}
+  API_ENDPOINTS: {
+    MAIN: {
+      DEFAULT: "/",
+    },
+    // user endpoints
+    USER_REGISTER: {
+      GET: "/users",
+      POST: "/users/register",
+      GET_UPDATE_DELETE_BY_ID: "/users/:id",
+    },
+    USER_LOGIN: {
+      GET: "/current",
+      POST: "/users/login",
+    },
+    // project endpoints
+    PROJECT: {
+      GET: "/projects",
+      POST: "/projects",
+      GET_UPDATE_DELETE_BY_ID: "/projects/:id",
+      ADD_MEMBER: "/projects/:project_id/member",
+    },
+    // project members endpoints
+    PROJECT_MEMBER: {
+      GET: "/:project_id",
+      DEACTIVATE: "/deactivate/:project_id/:user_id",
+      ACTIVATE: "/activate/:project_id/:user_id",
+    },
+    // stage endpoints
+    STAGE: {
+      GET: "/get/stages",
+      POST: "/stages",
+    },
+  },
+};
